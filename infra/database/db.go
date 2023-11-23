0bdf511b89896af27e01716e69b44f7e5af94de1
Ronaldnur <ronaldnursunarto@gmail.com>
2023-11-23 08:23:58 +0700

    <pesan commit>

--- infra/database/db.go
+++ infra/database/db.go
@@ -10,7 +10,7 @@ import (
 
 const (
 	host     = "localhost"
-	port     = 5432
+	port     = "5432"
 	user     = "postgres"
 	password = "postgres"
 	dbname   = "todos"
@@ -23,7 +23,7 @@ var (
 )
 
 func handleDatabaseConnection() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
+	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname,
 	)
 	db, err = sql.Open(dialect, psqlInfo)
